87182ddd503c3e7d38ce09d3879bf1198537cc43
suojae <suojae.developer@gmail.com>
2024-10-18 22:08:52 +0900

    chore: KafkaModule 동적 설정 추가

--- back_finding_nemo/kafka/kafka.module.ts
+++ back_finding_nemo/kafka/kafka.module.ts
@@ -1,26 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { KafkaService } from './kafka.service';
 import { KAFKA_BROKER } from './kafka.constants';
 
-@Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'KAFKA_CLIENT',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: [KAFKA_BROKER],
-          },
-          consumer: {
-            groupId: 'my-consumer-group',
-          },
-        },
-      },
-    ]),
-  ],
-  providers: [KafkaService],
-  exports: [KafkaService],
-})
-export class KafkaModule {}
+@Module({})
+export class KafkaModule {
+    static forRoot(groupId: string): DynamicModule {
+        return {
+            module: KafkaModule,
+            imports: [
+                ClientsModule.register([
+                    {
+                        name: 'KAFKA_CLIENT',
+                        transport: Transport.KAFKA,
+                        options: {
+                            client: {
+                                brokers: [KAFKA_BROKER],
+                            },
+                            consumer: {
+                                groupId,
+                            },
+                        },
+                    },
+                ]),
+            ],
+            providers: [KafkaService],
+            exports: [KafkaService],
+        };
+    }
+}
