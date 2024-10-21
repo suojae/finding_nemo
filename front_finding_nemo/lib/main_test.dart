e46c5c1eefa9f125d3794fe205406bafa398305e
suojae <suojae.developer@gmail.com>
2024-10-21 19:07:57 +0900

    feat: 테스트용 wave animation 추가

---
+++ front_finding_nemo/lib/main_test.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+// import 'package:front_finding_nemo/signin_page/signin_view.dart';
+// import 'package:google_fonts/google_fonts.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         textTheme: GoogleFonts.neuchaTextTheme(),
+//         splashFactory: NoSplash.splashFactory,
+//         highlightColor: Colors.transparent,
+//         hoverColor: Colors.transparent,
+//       ),
+//       home: SignInView(),
+//     );
+//   }
+// }
