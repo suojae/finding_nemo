b9d5db1de8efefca1c273dab0298f047466f2ad0
suojae <suojae.developer@gmail.com>
2024-10-21 17:01:10 +0900

    chore: frosted card 외부 터치 제스쳐 무시

--- front_finding_nemo/lib/main_view.dart
+++ front_finding_nemo/lib/main_view.dart
@@ -11,14 +11,23 @@ final class MainView extends StatelessWidget {
         children: [
           FlowFieldScreen(),
 
+          // Blurred backdrop
           BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 2.8, sigmaY: 2.8),
             child: Container(
               color: Colors.white.withOpacity(0.05),
             ),
           ),
+
+          Positioned.fill(
+            child: IgnorePointer(
+              ignoring: true, // Disable touch events for everything outside FrostedCard
+              child: Container(),
+            ),
+          ),
+
           Center(
-            child: FrostedCard(),
+            child: FrostedCard(), // Touch events inside FrostedCard are allowed
           ),
         ],
       ),
