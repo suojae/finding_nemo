6be4185830193d3b9cab41e0b27d986d7591b30b
suojae <suojae.developer@gmail.com>
2024-10-22 12:10:31 +0900

    chore: signin_view에 카드 및 네비게이션 로직 추가

--- front_finding_nemo/lib/signin_page/signin_view.dart
+++ front_finding_nemo/lib/signin_page/signin_view.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'flow_field.dart';
-import 'frosted_card.dart';
+import 'signin_card.dart';
+import 'signup_view.dart';
 
-final class SignInView extends StatelessWidget {
+class SignInView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           FlowFieldScreen(),
-
           BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 2.8, sigmaY: 2.8),
             child: Container(
               color: Colors.white.withOpacity(0.05),
             ),
           ),
-
           Positioned.fill(
             child: IgnorePointer(
               ignoring: true,
               child: Container(),
             ),
           ),
-
           Center(
-            child: FrostedCard(),
+            child: SignInCard(
+              onSignUpTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpView()),
+                );
+              },
+            ),
           ),
         ],
       ),
